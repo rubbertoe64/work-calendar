0ba7496ec9914897616c243390590026e90ecf47
Roberto Gonzalez <rgonzalez7359@gmail.com>
2022-01-16 17:48:50 -0800

    added comments to javascript

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+// this variable grabs todays day/date/year/time and displays it to the html
 var today = moment();
 $('#currentDay').text(today.format('llll'))
 
+// grabs the current time but only the Hour, and gets parsed into an object
 var now = parseInt(moment().format('H'));
-
+// initialize object
 var workDayObj = {}
-
+// adds the input value to the current time and updates page
 $('#changeBtn').click(function(e){
     e.preventDefault()
     var time = parseInt($('#change-time').val())
@@ -13,12 +15,16 @@ $('#changeBtn').click(function(e){
     init()
 })
 
-// console.log('now',now);
 
+// runs the operation that sets the work data
 function init(){
+    // used ternary operators to check if local storage has data,
+    // else sets it as empty object
     workDayObj = localStorage.getItem('work-day-data') ? JSON.parse(localStorage.getItem('work-day-data')) : {}
-    console.log(workDayObj);
+    
     const timeblockChildren = $('#time-blocks')[0].children;
+    // loop to get every time-block with "if" statement to switch colors
+    // depending on condition
     for(var i = 0; i < timeblockChildren.length; i++){
         
         var currentTimeBlock = timeblockChildren[i].children[1];
@@ -47,10 +53,11 @@ function init(){
             saveData(blockTime, blockText)
         })
     }
+    // saves to local storage as a string
     function saveData(time, text){
 
         workDayObj[time] = text;
-        console.log(workDayObj);
+        
 
         localStorage.setItem('work-day-data', JSON.stringify(workDayObj))
         
